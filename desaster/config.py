f4b8f5a1ce673e6e88e97b1103114bc09c59089d
dhuling <rockgod43@gmail.com>
2016-02-10 14:30:55 -0800

    add config file that changes the distributions of time parameters

---
+++ desaster/config.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 10 12:45:29 2016
+
+@author: Derek
+"""
+
+#configs
+
+import random
+
+inspection_time = random.gauss(1, 0)
+
+adjuster_time = random.gauss(1, 0)
+
+fema_process_time = random.gauss(2, 0)
+
+engineering_assessment_time = random.gauss(1, 0)
