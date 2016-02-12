5fa01bf4fef168b985b486ced3db005154c615d7
dhuling <rockgod43@gmail.com>
2016-02-11 16:37:44 -0800

    Add a view function to call the current settings of the simulation.

--- desaster/io.py
+++ desaster/io.py
@@ -5,3 +5,19 @@ Created on Wed Jan 20 14:14:36 2016
 @author: Derek
 """
 
+from desaster import config
+
+def view_config():
+    var = vars(config)
+    conf = dir(config)
+    result = {}
+    for i in conf:
+        if "__" not in i and "random" not in i:
+            print ("{} = {}".format(i, var[i]))
+            #print (i)
+            result[i] = var[i]
+            
+        else:
+            pass
+    return result
+        
