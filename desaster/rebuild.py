917d537010573dc9f53a6a0c46c0ca9ee0f2e5d3
geomando <smiles@mailworks.org>
2016-07-06 14:31:40 -0700

    Changed rebuild_house() to rebuild_home(), added interrupt handling to it

--- desaster/rebuild.py
+++ desaster/rebuild.py
@@ -10,8 +10,8 @@ processes related to rebuilding
 from desaster.config import building_repair_times
 from simpy import Interrupt
 
-def rebuild_house(simulation, human_capital, entity, story = True): 
-        
+def rebuild_home(simulation, human_capital, entity, story = True, callbacks = None): 
+    try: # in case a process interrupt is thrown in a master process
         if entity.permit_get > 0.0 and entity.money_to_rebuild >= entity.residence.damage_value:
             # Put in request for contractors to repair house
             entity.house_put = simulation.now
@@ -19,14 +19,14 @@ def rebuild_house(simulation, human_capital, entity, story = True):
             request = human_capital.contractors.request()
             yield request
             
-            # Get the rebuild time for the entity from config.py which imports the HAZUS repair time look up table
+            # Get the rebuild time for the entity from config.py 
+            # which imports the HAZUS repair time look up table.
+            # Rebuild time is based on occupancy type and damage state.
             rebuild_time = building_repair_times.ix[entity.residence.occupancy][entity.residence.damage_state]
             
             yield simulation.timeout(rebuild_time)
         
-
             human_capital.contractors.release(request)
-
 
             # Record time when household gets house
             entity.house_get = simulation.now
@@ -47,3 +47,16 @@ def rebuild_house(simulation, human_capital, entity, story = True):
                 entity.story.append(
                     '{0} was unable to get a permit to rebuild. '.format(
                     entity.name))
+
+    except Interrupt as i: # Handle any interrupt thrown by a master process
+        if story == True:
+            #If true, write their story
+            entity.story.append(
+                    '{0} gave up {1} days into the home rebuilding process. '.format(
+                    entity.name, i.cause))
+    
+    if callbacks is not None:
+        yield simulation.process(callbacks)
+
+    else:
+        pass
